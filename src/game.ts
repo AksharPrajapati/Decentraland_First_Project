89bb102bce48dea4cfc4853e7f633959320f38d3
Akshar Prajapati <aksharpp369@gmail.com>
2022-04-22 10:47:36 +0530

    try to hide default avatar and show our custom

--- src/game.ts
+++ src/game.ts
@@ -3,15 +3,15 @@ import { Arissa } from "./arissa";
 
 // Base
 const base = new Entity();
-base.addComponent(new GLTFShape("models/baseGrass.glb"));
+base.addComponent(new GLTFShape("models/scene.glb"));
 engine.addEntity(base);
 
 // Arissa
 const arissa = new Arissa(
   new GLTFShape("models/run204.glb"),
   new Transform({
-    position: new Vector3(0, 0.05, -0.1),
-    scale: new Vector3(0, 0, 0),
+    position: new Vector3(0, -0.85, -0.1),
+    scale: new Vector3(1, 1, 1),
   })
 );
 arissa.setParent(Attachable.AVATAR);
@@ -20,29 +20,29 @@ arissa.setParent(Attachable.AVATAR);
 const hideAvatarsEntity = new Entity();
 hideAvatarsEntity.addComponent(
   new AvatarModifierArea({
-    area: { box: new Vector3(16, 4, 11) },
+    area: { box: new Vector3(1000, 1000, 1000) },
     modifiers: [AvatarModifiers.HIDE_AVATARS],
   })
 );
-hideAvatarsEntity.addComponent(
-  new Transform({ position: new Vector3(8, 2, 10.5) })
-);
+// hideAvatarsEntity.addComponent(
+//   new Transform({ position: new Vector3(7.41, 0.88, 16) })
+// );
 engine.addEntity(hideAvatarsEntity);
 
 // Create to show Arissa avatar
-hideAvatarsEntity.addComponent(
-  new utils.TriggerComponent(
-    new utils.TriggerBoxShape(new Vector3(16, 4, 11), Vector3.Zero()),
-    {
-      onCameraEnter: () => {
-        arissa.getComponent(Transform).scale.setAll(1);
-      },
-      onCameraExit: () => {
-        arissa.getComponent(Transform).scale.setAll(0);
-      },
-    }
-  )
-);
+// hideAvatarsEntity.addComponent(
+//   new utils.TriggerComponent(
+//     new utils.TriggerBoxShape(new Vector3(100, 4, 100), Vector3.Zero()),
+//     {
+//       onCameraEnter: () => {
+//         arissa.getComponent(Transform).scale.setAll(1);
+//       },
+//       onCameraExit: () => {
+//         arissa.getComponent(Transform).scale.setAll(0);
+//       },
+//     }
+//   )
+// );
 
 // Check if player is moving
 const currentPosition = new Vector3();
